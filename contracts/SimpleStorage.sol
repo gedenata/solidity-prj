c0197022a7acdb1871549b86c508631e3024cf1b
gedenata <igedetirtanata@gmail.com>
2022-04-17 00:05:59 +0800

    chore: create file .sol 'SimpleStorage'

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+abstract contract SimpleStorage {
+    uint256 public storedData;
+
+    constructor() {
+        storedData = 0;
+    }
+
+    function set(uint256 x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
