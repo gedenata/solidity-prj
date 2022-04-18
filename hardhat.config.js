b10c7f1265f1a4b7c5e5b7acf56e07c40499e3e3
gedenata <igedetirtanata@gmail.com>
2022-04-18 18:46:08 +0800

    chore: add dependency hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,34 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
-// require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-etherscan");
 
 // require("hardhat-deploy");
 // require("hardhat-gas-reporter");
 // require("hardhat-contract-sizer");
 
 require("solidity-coverage");
-
-// const { extendEnvironment, task } = require("hardhat/config");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-//   const accounts = await hre.ethers.getSigners();
-
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
-
-// extendEnvironment((hre) => {
-//   hre.hi = "Hello, Hardhat!";
-// });
-
-// task("envtest", async (taskArgs, hre) => {
-//   console.log(hre.hi);
-// });
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 const MAINNET_RPC_URL =
   process.env.MAINNET_RPC_URL ||
@@ -39,9 +16,6 @@ const MNEMONIC = process.env.MNEMONIC || "your_mnemonic";
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const FORKING_BLOCK_NUMBER = process.env.FORKING_BLOCK_NUMBER;
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
